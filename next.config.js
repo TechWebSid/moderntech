d9d93db29b2f36bb95ea9e7636df79ac9f2df4c9
TechWebSid <siddharthasrivastava30@gmail.com>
2025-01-28 15:52:02 +0530

    ..

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     remotePatterns: [
       {
@@ -22,6 +23,6 @@ const nextConfig = {
       }
     ],
   },
-}
+  }
 
 module.exports = nextConfig 
